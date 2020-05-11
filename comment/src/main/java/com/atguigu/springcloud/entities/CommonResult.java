08c81ab0bbf62f88ab23ba7de561a8d77c63f893
黎兵兵 <1587167182@qq.com>
2020-05-12 00:00:27 +0800

    [request] payment 自测成功

--- comment/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
+++ comment/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -1,8 +1,6 @@
 package com.atguigu.springcloud.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 /**
  * @Author libingbing
@@ -10,18 +8,22 @@ import lombok.NoArgsConstructor;
  * @Description 返回类
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class CommonResult<T> {
-
     private Integer code;
     private String message;
     private T data;
 
     public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+
+    public CommonResult(Integer code, String message, T data) {
         this.code = code;
         this.message = message;
-        this.data = null;
+        this.data = data;
+    }
+
+    public CommonResult() {
     }
 
 }
---
+++ payment8001/src/main/java/com/atguigu/springcloud/Controller/PaymentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.springcloud.Controller;
+
+import com.atguigu.springcloud.Service.PaymentService;
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author libingbing
+ * @Date 2020-05-11 21:50
+ * @Description
+ */
+@RestController
+@Slf4j
+@RequestMapping("/cloud2020/payment")
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody Payment payment) {
+
+        int i = paymentService.create(payment);
+        if (i > 0) {
+            return new CommonResult(200, "插入成功");
+        }
+        return new CommonResult(400, "插入失败");
+    }
+
+    @GetMapping("/findById")
+    public CommonResult<Payment> findById( Long id) {
+        Log log = LogFactory.getLog(PaymentController.class);
+        log.info("payment:"+paymentService.findById(id));
+        return new CommonResult(200, "查询成功", paymentService.findById(id));
+    }
+
+}
