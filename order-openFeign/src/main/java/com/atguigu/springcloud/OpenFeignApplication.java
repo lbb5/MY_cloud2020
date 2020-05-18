65049a779020c1aac79b3f19f9f1a5ba6b857315
黎兵兵 <1587167182@qq.com>
2020-05-18 09:19:48 +0800

    open-Feign

---
+++ order-openFeign/src/main/java/com/atguigu/springcloud/OpenFeignApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author libingbing
+ * @Date 2020-05-18 8:53
+ * @Description
+ */
+@SpringBootApplication
+public class OpenFeignApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignApplication.class,args);
+    }
+}
