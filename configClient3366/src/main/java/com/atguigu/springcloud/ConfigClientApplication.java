d8aa0eadcf92c09d3cda51cbc74110cabde096ad
黎兵兵 <1587167182@qq.com>
2020-05-20 08:57:24 +0800

    bus和rabbitMq结合，进行广播通知

---
+++ configClient3366/src/main/java/com/atguigu/springcloud/ConfigClientApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author libingbing
+ * @Date 2020-05-20 8:29
+ * @Description
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class,args);
+    }
+}
