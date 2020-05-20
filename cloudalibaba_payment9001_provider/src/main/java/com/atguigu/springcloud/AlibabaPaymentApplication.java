69e3a801a187d41b73d4a4f3eb4e9240f3e37c7b
黎兵兵 <1587167182@qq.com>
2020-05-20 16:26:40 +0800

    cloudAlibaba

---
+++ cloudalibaba_payment9001_provider/src/main/java/com/atguigu/springcloud/AlibabaPaymentApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author libingbing
+ * @Date 2020-05-20 16:13
+ * @Description
+ */
+@SpringBootApplication
+public class AlibabaPaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaPaymentApplication.class,args);
+    }
+}
---
+++ cloudalibaba_payment9002_provider/src/main/java/com/atguigu/springcloud/CloudPaymentApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author libingbing
+ * @Date 2020-05-20 16:20
+ * @Description
+ */
+@SpringBootApplication
+public class CloudPaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPaymentApplication.class,args);
+    }
+}
